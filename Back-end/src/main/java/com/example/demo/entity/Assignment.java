7cc301047bd50954a9ad15c6243f9fbe35f263c0
yanissaouri <yaniss.aouri@laplateforme.io>
2026-09-09 14:43:53 +0200

    modified the structure

--- Back-end/src/main/java/com/example/demo/entity/Assignment.java
+++ Back-end/src/main/java/com/example/demo/entity/Assignment.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Null;
--- Back-end/src/main/java/com/example/demo/repository/AssignmentRepository.java
+++ Back-end/src/main/java/com/example/demo/repository/AssignmentRepository.java
@@ -1,6 +1,8 @@
-package com.example.demo;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Assignment;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
 }
