318ef81a233cc3bb741c21bb17713e9aef94f722
yanissaouri <yaniss.aouri@laplateforme.io>
2026-09-09 21:12:20 +0200

    feat: worked on the controller, gotta wait for the service to be done to make it work

---
+++ Back-end/src/main/java/com/example/demo/service/AdventurerService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.Adventurer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdventurerService {
+
+    public List<Adventurer> getAllAdventurers(){
+        return null;
+    }
+}
